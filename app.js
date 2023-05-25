7e056f7002e901314ad62d06cca2448506f78f88
Soban142 <inferiorfoe@gmail.com>
2023-05-25 17:34:41 +0500

    completed

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ var modal = document.querySelector('.modal')
 var main = document.querySelector('.main')
 var hidden = document.querySelector('.hidden')
 var modalbtn = document.querySelector('.btn')
+var overlay = document.getElementsByClassName('overlay')
 var closebtn = document.querySelector('.closebtn')
 
 modalbtn.addEventListener('click', () => {
     modal.classList.remove('hidden')
     main.classList.add('hidden')
+    overlay[0].classList.remove('hidden')
+
 })
 
 closebtn.addEventListener('click', () => {
     modal.classList.add('hidden')
     main.classList.remove('hidden')
+    overlay[0].classList.add('hidden')
 })
